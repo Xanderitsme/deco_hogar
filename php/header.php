5561df641857b985e0b23dde954ddd1d5c570664
Xanderitsme <xanderitsme@gmail.com>
2023-12-02 23:00:52 -0500

    Se separó el header aparte en header.php

---
+++ php/header.php
@@ -0,0 +1,14 @@
+<header class="header">
+    <div class="titulo"><h1>Deco Hogar</h1></div>
+    <div class="carta-usuario borde sombra">
+        <div class="foto-usuario borde sombra">
+            <img src="" alt="">
+        </div>
+        <div class="nombre">
+            Juan Daniel Rojas Cevallos
+        </div>
+        <div class="cargo">
+            Vendedor
+        </div>
+    </div>
+</header>
