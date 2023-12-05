b82ec954b9eddab3544deb6042728cac934e2bf5
Xanderitsme <xanderitsme@gmail.com>
2023-12-05 11:27:24 -0500

    se agregó el icono para la página

--- app/views/inc/head.php
+++ app/views/inc/head.php
@@ -1,6 +1,7 @@
     <meta charset="UTF-8">
     <title><?php echo APP_NAME; ?></title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image/png" href="<?php echo APP_URL; ?>app/views/img/favicon/favicon.png">
     <link rel="stylesheet" href="<?php echo APP_URL; ?>app/views/css/bulma.min.css">
     <link rel="stylesheet" href="<?php echo APP_URL; ?>app/views/css/sweetalert2.min.css">
     <link rel="stylesheet" href="<?php echo APP_URL; ?>app/views/css/estilos.css">
