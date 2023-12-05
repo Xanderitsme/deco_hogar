f7430be3498f4ee74e3aa8e4fdc51b9ec8fb406a
Xanderitsme <xanderitsme@gmail.com>
2023-12-05 11:24:57 -0500

    Controlador, vista y Ajax para el usuario

--- app/controllers/userController.php
+++ app/controllers/userController.php
@@ -1,8 +1,175 @@
 <?php
 namespace app\controllers;
 use app\models\mainModel;
+use PDO;
 
 class userController extends mainModel {
 
-    
+    public function registrarUsuarioControlador() {
+        
+        $trabajadorId = $this->limpiarCadena($_POST["usuario_trabajador_id"]);
+        $usuario = $this->limpiarCadena($_POST["usuario_usuario"]);
+        $clave1 = $this->limpiarCadena($_POST["usuario_clave_1"]);
+        $clave2 = $this->limpiarCadena($_POST["usuario_clave_2"]);
+
+        if (empty($trabajadorId) || empty($usuario) || empty($clave1) || empty($clave2)) {
+            $alerta = $this->crearAlertaError("No has llenado todos los campos que son obligatorios");
+            return json_encode($alerta);
+            exit();
+        }
+
+        if ($this->verificarDatos("[1-9]{1,4}", $trabajadorId)) {
+            $alerta = $this->crearAlertaError("El ID del trabajador no coincide con el formato solicitado");
+            return json_encode($alerta);
+            exit();
+        }
+
+        if ($this->verificarDatos("[a-zA-Z0-9]{4,20}", $usuario)) {
+            $alerta = $this->crearAlertaError("El usuario no coincide con el formato solicitado");
+            return json_encode($alerta);
+            exit();
+        }
+
+        if ($this->verificarDatos("[a-zA-Z0-9$@.-]{6,20}", $clave1) || $this->verificarDatos("[a-zA-Z0-9$@.-]{6,20}", $clave2)) {
+            $alerta = $this->crearAlertaError("Las claves no coinciden con el formato solicitado");
+            return json_encode($alerta);
+            exit();
+        }
+
+        if ($clave1 != $clave2) {
+            $alerta = $this->crearAlertaError("Las claves que acaba de ingresar no coinciden");
+            return json_encode($alerta);
+            exit();
+        } else {
+            $clave = password_hash($clave1, PASSWORD_BCRYPT, ["cost" => 10]);
+        }
+
+        $check_trabajadorId = $this->ejecutarConsulta("select ID from trabajadores where ID = '$trabajadorId'");
+
+        if (!is_null($check_trabajadorId)) {
+            if ($check_trabajadorId->rowCount() == 0) {
+                $alerta = $this->crearAlertaError("El trabajador ingresado no esta registrado");
+                return json_encode($alerta);
+                exit();
+            }
+        } else {
+            return $this->errorRegistro();
+            exit();
+        }
+
+        // Verificar si el trabajador ya tiene una cuenta registrada (opcional)
+
+        // $check_cuenta_registrada = $this->ejecutarConsulta("select trabajadorID from cuentas where trabajadorID = '$trabajadorId'");
+
+        // if (!is_null($check_cuenta_registrada)) {
+        //     if ($check_cuenta_registrada->rowCount() > 0) {
+        //         $alerta = $this->crearAlertaError("El trabajador ingresado ya tiene una cuenta registrada");
+        //         return json_encode($alerta);
+        //         exit();
+        //     }
+        // } else {
+        //     return $this->errorRegistro();
+        //     exit();
+        // }
+
+        $check_usuario = $this->ejecutarConsulta("select usuario from cuentas where usuario like '$usuario'");
+        if (!is_null($check_usuario)) {
+            if ($check_usuario->rowCount() > 0) {
+                $alerta = $this->crearAlertaError("El usuario que acaba de ingresar ya se encuentra registrado");
+                return json_encode($alerta);
+                exit();
+            }
+        } else {
+            return $this->errorRegistro();
+            exit();
+        }
+
+        $img_dir = "../views/fotos/";
+
+        if ($_FILES['usuario_foto']['name'] != "" && $_FILES['usuario_foto']['size'] > 0) {
+            if (!file_exists($img_dir)) {
+                if (!mkdir($img_dir,0777)) {
+                    $alerta = $this->crearAlertaError("Error al crear el directorio");
+                    return json_encode($alerta);
+                    exit();
+                } 
+            }
+
+            if (mime_content_type($_FILES['usuario_foto']['tmp_name']) != "image/jpeg" && mime_content_type($_FILES['usuario_foto']['tmp_name']) != "image/png") {
+                $alerta = $this->crearAlertaError("La imagen que ha seleccionado es de un formato no permitido");
+                return json_encode($alerta);
+                exit();
+            }
+
+            if (($_FILES['usuario_foto']['size'] / 1024) > 5120) {
+                $alerta = $this->crearAlertaError("La imagen que ha seleccionado supera el peso permitido");
+                return json_encode($alerta);
+                exit();
+            }
+
+            $foto = str_ireplace(" ", "_", $usuario);
+            $foto = $foto . "_" . rand(0,100);
+
+            switch (mime_content_type($_FILES['usuario_foto']['tmp_name'])) {
+                case 'image/jpeg':
+                    $foto = $foto . ".jpg";
+                    break;
+                case 'image/png':
+                    $foto = $foto . ".png";
+                    break;
+            }
+
+            chmod($img_dir,0777);
+
+            if (!move_uploaded_file($_FILES['usuario_foto']['tmp_name'], $img_dir . $foto)) {
+                $alerta = $this->crearAlertaError("No podemos subir la imagen al sistema en este momento");
+                return json_encode($alerta);
+                exit();
+            }
+        } else {
+            $foto = "";
+        }
+
+        $usuario_datos_reg = [
+            [
+                "campo_nombre" => "trabajadorId",
+                "campo_marcador" => ":trabajadorId",
+                "campo_valor" => $trabajadorId
+            ],
+            [
+                "campo_nombre" => "usuario",
+                "campo_marcador" => ":usuario",
+                "campo_valor" => $usuario
+            ],
+            [
+                "campo_nombre" => "clave",
+                "campo_marcador" => ":clave",
+                "campo_valor" => $clave
+            ],
+            [
+                "campo_nombre" => "foto",
+                "campo_marcador" => ":foto",
+                "campo_valor" => $foto
+            ],
+        ];
+
+        $registrarUsuario = $this->guardarDatos("cuentas", $usuario_datos_reg);
+
+        if (!is_null($registrarUsuario)) {
+            if ($registrarUsuario->rowCount() == 1) {
+                $alerta = $this->crearAlertaSuccess("Usuario registrado", "El usuario " . $usuario . "ha sido registrado exitosamente");
+            } else {
+                $alerta = $this->crearAlertaError("No se pudo registrar el usuario, por favor intente nuevamente");
+            }
+        } else {
+            $alerta = $this->crearAlertaError("No se pudo registrar el usuario, por favor intente más tarde");
+        }
+
+        return json_encode($alerta);
+    }
+
+    private function errorRegistro() {
+        $alerta = $this->crearAlertaError("Ha ocurrido un error al intentar registrar los datos del usuario");
+        return json_encode($alerta);
+    }
 }
---
+++ app/views/content/nuevoUsuario-view.php
@@ -0,0 +1,66 @@
+<div class="borde"></div>
+<div class="container is-fluid mb-6">
+	<h1 class="title pt-6">Usuarios</h1>
+</div>
+<div class="container pb-6 pt-6">
+	<h2 class="subtitle">Registrar usuarios</h2>
+	<form class="FormularioAjax" action="<?php echo APP_URL; ?>app/ajax/trabajadorAjax.php" method="POST" autocomplete="off" enctype="multipart/form-data" >
+		<input type="hidden" name="nuevo_trabajador" value="registrar">
+		<div class="columns">
+		  	<div class="column">
+		    	<div class="control">
+					<label>
+						ID trabajador *
+						<input class="input" type="text" name="usuario_trabajador_id" pattern="[1-9]{1,4}" maxlength="4" required >
+					</label>
+				</div>
+		  	</div>
+		  	<div class="column">
+		    	<div class="control">
+					<label>
+						Nombre de usuario *
+						<input class="input" type="text" name="usuario_usuario" pattern="[a-zA-Z0-9]{4,20}" maxlength="20" required >
+					</label>
+				</div>
+		  	</div>
+		</div>
+		<div class="columns">
+			<div class="column">
+				<div class="control">
+					<label>
+						clave *
+						<input class="input" type="text" name="usuario_clave_1" pattern="[a-zA-Z0-9$@.-]{6,20}" maxlength="20" required >
+					</label>
+				</div>
+			</div>
+			<div class="column">
+		    	<div class="control">
+					<label>
+						Repetir clave *
+						<input class="input" type="text" name="usuario_clave_2" pattern="[a-zA-Z0-9$@.-]{6,20}" maxlength="20" required >
+					</label>
+				</div>
+		  	</div>
+		</div>
+		<div class="columns">
+		  	<div class="column">
+				<div class="file has-name is-boxed">
+					<label class="file-label">
+						<input class="file-input" type="file" name="usuario_foto" accept=".jpg, .png, .jpeg" >
+						<span class="file-cta">
+							<span class="file-label">
+								Seleccione una foto
+							</span>
+						</span>
+						<span class="file-name">JPG, JPEG, PNG. (MAX 5MB)</span>
+					</label>
+				</div>
+		  	</div>
+		</div>
+		<span>* Obligatorio</span>
+		<p class="has-text-centered">
+			<button type="reset" class="button is-link is-light is-rounded">Limpiar</button>
+			<button type="submit" class="button is-info is-rounded">Guardar</button>
+		</p>
+	</form>
+</div>
