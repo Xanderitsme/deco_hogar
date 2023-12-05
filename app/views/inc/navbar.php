f2fe870eac61df2107a303efa08031462a3a88d0
Xanderitsme <xanderitsme@gmail.com>
2023-12-05 11:27:52 -0500

    se añadieron más enlaces a la navbar

--- app/views/inc/navbar.php
+++ app/views/inc/navbar.php
@@ -1,7 +1,7 @@
 <nav class="navbar">
     <div class="navbar-brand">
         <a class="navbar-item" href="<?php echo APP_URL; ?>dashboard/">
-            <div><h1 class="title">Deco Hogar</h1></div>
+            <div><h1 class="title" style="font-weight: 700;">Deco Hogar</h1></div>
         </a>
         <div class="navbar-burger" data-target="navbarExampleTransparentExample">
             <span></span>
@@ -15,12 +15,65 @@
             <a class="navbar-item" href="<?php echo APP_URL; ?>dashboard/">
                 Dashboard
             </a>
-            <a class="navbar-item" href="<?php echo APP_URL; ?>productos/">
-                Productos
-            </a>
-            <a class="navbar-item" href="<?php echo APP_URL; ?>clientes/">
-                Clientes
-            </a>
+            <div class="navbar-item has-dropdown is-hoverable">
+                <a class="navbar-link" href="<?php echo APP_URL; ?>productos/">
+                    Productos
+                </a>
+                <div class="navbar-dropdown is-boxed">
+                    <a class="navbar-item" href="#" id="btn_exit" >
+                        Nuevo producto
+                    </a>
+                    <a class="navbar-item" href="#" id="btn_exit" >
+                        Nuevo producto
+                    </a>
+                </div>
+            </div>
+            <div class="navbar-item has-dropdown is-hoverable">
+                <a class="navbar-link" href="<?php echo APP_URL; ?>clientes/">
+                    Clientes
+                </a>
+                <div class="navbar-dropdown is-boxed">
+                    <a class="navbar-item" href="#" id="btn_exit" >
+                        Nuevo cliente
+                    </a>
+                </div>
+            </div>
+            <div class="navbar-item has-dropdown is-hoverable">
+                <a class="navbar-link" href="#">
+                    Proformas de venta
+                </a>
+                <div class="navbar-dropdown is-boxed">
+                    <a class="navbar-item" href="#" id="btn_exit" >
+                        Nueva proforma de venta
+                    </a>
+                </div>
+            </div>
+            <div class="navbar-item has-dropdown is-hoverable">
+                <a class="navbar-link" href="#">
+                    Inventario
+                </a>
+                <div class="navbar-dropdown is-boxed">
+                    <a class="navbar-item" href="#" id="btn_exit" >
+                        Historial de movimientos de inventario
+                    </a>
+                </div>
+            </div>
+            <div class="navbar-item has-dropdown is-hoverable">
+                <a class="navbar-link" href="#">
+                    Trabajadores
+                </a>
+                <div class="navbar-dropdown is-boxed">
+                    <a class="navbar-item" href="<?php echo APP_URL; ?>nuevoTrabajador/" id="btn_exit" >
+                        Registrar trabajador
+                    </a>
+                    <a class="navbar-item" href="#" id="btn_exit" >
+                        Usuarios
+                    </a>
+                    <a class="navbar-item" href="<?php echo APP_URL; ?>nuevoUsuario/" id="btn_exit" >
+                        Registrar usuario
+                    </a>
+                </div>
+            </div>
         </div>
         
         <div class="navbar-end">
