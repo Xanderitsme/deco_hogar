8eb6c7661e6c57eef223ecee8535ffa32705b4eb
Xanderitsme <xanderitsme@gmail.com>
2023-12-06 15:32:46 -0500

    Se agregó la imagen de perfil del usuario

--- app/views/content/dashboard-view.php
+++ app/views/content/dashboard-view.php
@@ -2,11 +2,17 @@
 	<div class="columns is-flex is-justify-content-center">
 		<div class="contenedor-imagen">
 			<figure class="imagen">
-				<img src="<?php echo APP_URL ?>app/views/fotos/default.png" alt="foto de perfil del usuario">
+				<?php
+					if (is_file("app/views/fotos/" . $_SESSION["foto"])) {
+						echo '<img src="' . APP_URL . 'app/views/fotos/' . $_SESSION["foto"] .'" alt="foto de perfil del usuario">';
+					} else {
+						echo '<img src="' . APP_URL . 'app/views/fotos/default.png" alt="foto de perfil del usuario">';
+					}
+				?>
 			</figure>
 		</div>
 	</div>
-	<div class="columns is-flex is-justify-content-center">
-		<h2 class="subtitle">¡Bienvenido <?php echo "Username"; ?>!</h2>
+	<div class="columns is-flex is-justify-content-center mt-3">
+		<h2 class="subtitle">¡Bienvenido <?php echo $_SESSION['nombres'] . " " . $_SESSION['apellidos']; ?>!</h2>
 	</div>
 </div>
