f9cc2ade847b81c14e4e603d59dbc0695c871518
Xanderitsme <xanderitsme@gmail.com>
2023-12-06 15:33:57 -0500

    Cambio de nombre de la aplicación

--- config/app.php
+++ config/app.php
@@ -1,6 +1,6 @@
 <?php
 const APP_URL = "http://localhost/deco_hogar/";
-const APP_NAME = "Deco Hogar App";
+const APP_NAME = "Deco Hogar";
 const APP_SESION_NAME = "APP";
 
 date_default_timezone_set("America/Lima");
