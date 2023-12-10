bb6881c13a5255328bdb69c9f7391dc7b7e75854
Xanderitsme <xanderitsme@gmail.com>
2023-12-09 23:57:46 -0500

    se agregó la vista para listar y actualizar trabajadores

---
+++ app/views/content/listaTrabajadores-view.php
@@ -0,0 +1,17 @@
+<div class="borde"></div>
+<div class="container is-fluid mt-6">
+	<h1 class="title">Trabajadores</h1>
+	<h2 class="subtitle">Lista de trabajadores</h2>
+</div>
+<div class="container is-fluid pb-6 pt-6" style="padding-left: 6rem; padding-right: 6rem;">
+
+	<div class="form-rest mb-6 mt-6"></div>
+
+	<?php
+		use app\controllers\usuarioController;
+
+		$insUsuario = new usuarioController();
+
+		echo $insUsuario->listarTrabajadoresControlador($url[1], 10, $url[0], "");
+	?>
+</div>
