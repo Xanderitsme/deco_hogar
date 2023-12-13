0265d9ea5b4bf0bc409f4328db99d04f959f6f2c
Xanderitsme <xanderitsme@gmail.com>
2023-12-13 07:10:05 -0500

    Se cambió la identación de 4 a 2 espacios

--- app/models/mainModel.php
+++ app/models/mainModel.php
@@ -6,278 +6,301 @@ use Exception;
 use PDO;
 
 if (file_exists(__DIR__ . "/../../config/server.php")) {
-    require_once __DIR__ . "/../../config/server.php";
+  require_once __DIR__ . "/../../config/server.php";
 }
 
 class mainModel
 {
 
-    private $server = DB_SERVER;
-    private $db = DB_NAME;
-    private $user = DB_USER;
-    private $pass = DB_PASS;
+  private $server = DB_SERVER;
+  private $db = DB_NAME;
+  private $user = DB_USER;
+  private $pass = DB_PASS;
 
-    protected function conectar()
-    {
-        try {
-            $conexion = new PDO("mysql:host=" . $this->server . ";dbname=" . $this->db, $this->user, $this->pass);
-            $conexion->exec("SET CHARACTER SET utf8");
-            $conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            return $conexion;
-        } catch (Exception $ex) {
-            echo $ex->getMessage();
-        }
+  protected function conectar()
+  {
+    try {
+      $conexion = new PDO("mysql:host=" . $this->server . ";dbname=" . $this->db, $this->user, $this->pass);
+      $conexion->exec("SET CHARACTER SET utf8");
+      $conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+      return $conexion;
+    } catch (Exception $ex) {
+      echo $ex->getMessage();
+    }
+  }
+
+  protected function ejecutarConsulta($consulta)
+  {
+    try {
+      $sql = $this->conectar()->prepare($consulta);
+      $sql->execute();
+    } catch (Exception $e) {
+      // echo "Error: " . $e;
+      $sql = null;
     }
 
-    protected function ejecutarConsulta($consulta)
-    {
-        try {
-            $sql = $this->conectar()->prepare($consulta);
-            $sql->execute();
-        } catch (Exception $e) {
-            // echo "Error: " . $e;
-            $sql = null;
-        }
+    return $sql;
+  }
 
-        return $sql;
+  public function limpiarCadena($cadena)
+  {
+    $palabras = [
+      "<script>",
+      "</script>",
+      "<script src",
+      "<script type=",
+      "SELECT * FROM",
+      "SELECT ",
+      " SELECT ",
+      "DELETE FROM",
+      "INSERT INTO",
+      "DROP TABLE",
+      "DROP DATABASE",
+      "TRUNCATE TABLE",
+      "SHOW TABLES",
+      "SHOW DATABASES",
+      "<?php",
+      "?>",
+      "--",
+      "^",
+      "<",
+      ">",
+      "==",
+      "=",
+      ";",
+      "::"
+    ];
+
+    $cadena = trim($cadena);
+    $cadena = stripslashes($cadena);
+
+    foreach ($palabras as $palabra) {
+      $cadena = str_ireplace($palabra, "", $cadena);
     }
 
-    public function limpiarCadena($cadena)
-    {
-        $palabras = [
-            "<script>",
-            "</script>",
-            "<script src",
-            "<script type=",
-            "SELECT * FROM",
-            "SELECT ",
-            " SELECT ",
-            "DELETE FROM",
-            "INSERT INTO",
-            "DROP TABLE",
-            "DROP DATABASE",
-            "TRUNCATE TABLE",
-            "SHOW TABLES",
-            "SHOW DATABASES",
-            "<?php",
-            "?>",
-            "--",
-            "^",
-            "<",
-            ">",
-            "==",
-            "=",
-            ";",
-            "::"
-        ];
+    return $cadena;
+  }
 
-        $cadena = trim($cadena);
-        $cadena = stripslashes($cadena);
-
-        foreach ($palabras as $palabra) {
-            $cadena = str_ireplace($palabra, "", $cadena);
-        }
-
-        return $cadena;
+  protected function verificarDatos($filtro, $cadena)
+  {
+    if (preg_match("/^" . $filtro . "$/", $cadena)) {
+      return false;
     }
 
-    protected function verificarDatos($filtro, $cadena)
-    {
-        if (preg_match("/^" . $filtro . "$/", $cadena)) {
-            return false;
-        }
+    return true;
+  }
 
-        return true;
+  protected function guardarDatos($tabla, $datos)
+  {
+
+    $query = "insert into $tabla (";
+
+    $contador = 0;
+    foreach ($datos as $clave) {
+      if ($contador >= 1) {
+        $query .= ",";
+      }
+      $query .= $clave["campo_nombre"];
+      $contador++;
     }
 
-    protected function guardarDatos($tabla, $datos)
-    {
+    $query .= ") values(";
 
-        $query = "insert into $tabla (";
-
-        $contador = 0;
-        foreach ($datos as $clave) {
-            if ($contador >= 1) {
-                $query .= ",";
-            }
-            $query .= $clave["campo_nombre"];
-            $contador++;
-        }
-
-        $query .= ") values(";
-
-        $contador = 0;
-        foreach ($datos as $clave) {
-            if ($contador >= 1) {
-                $query .= ",";
-            }
-            $query .= $clave["campo_marcador"];
-            $contador++;
-        }
-
-        $query .= ")";
-
-        try {
-            $sql = $this->conectar()->prepare($query);
-            foreach ($datos as $clave) {
-                $sql->bindParam($clave["campo_marcador"], $clave["campo_valor"]);
-            }
-            $sql->execute();
-        } catch (Exception) {
-            $sql = null;
-        }
-
-        return $sql;
+    $contador = 0;
+    foreach ($datos as $clave) {
+      if ($contador >= 1) {
+        $query .= ",";
+      }
+      $query .= $clave["campo_marcador"];
+      $contador++;
     }
 
-    public function seleccionarDatos($tipo, $tabla, $campo, $id)
-    {
-        $tipo = $this->limpiarCadena($tipo);
-        $tabla = $this->limpiarCadena($tabla);
-        $campo = $this->limpiarCadena($campo);
-        $id = $this->limpiarCadena($id);
+    $query .= ")";
 
-        try {
-            if ($tipo == "Unico") {
-                $sql = $this->conectar()->prepare("select * from $tabla where $campo = :id");
-                $sql->bindParam(":id", $id, PDO::PARAM_INT);
-            } elseif ($tipo == "Normal") {
-                $sql = $this->conectar()->prepare("select $campo from $tabla");
-            }
-
-            $sql->execute();
-        } catch (Exception) {
-            $sql = null;
-        }
-
-        return $sql;
+    try {
+      $sql = $this->conectar()->prepare($query);
+      foreach ($datos as $clave) {
+        $sql->bindParam($clave["campo_marcador"], $clave["campo_valor"]);
+      }
+      $sql->execute();
+    } catch (Exception) {
+      $sql = null;
     }
 
-    protected function actualizarDatos($tabla, $datos, $condicion)
-    {
-        $query = "update $tabla set ";
+    return $sql;
+  }
 
-        $contador = 0;
-        foreach ($datos as $clave) {
-            if ($contador >= 1) {
-                $query .= ",";
-            }
-            $query .= $clave["campo_nombre"] . "=" . $clave["campo_marcador"];
-            $contador++;
-        }
+  public function seleccionarDatos($tipo, $tabla, $campo, $id)
+  {
+    $tipo = $this->limpiarCadena($tipo);
+    $tabla = $this->limpiarCadena($tabla);
+    $campo = $this->limpiarCadena($campo);
+    $id = $this->limpiarCadena($id);
 
-        $query .= " where " . $condicion["condicion_campo"] . "=" . $condicion["condicion_marcador"];
+    try {
+      if ($tipo == "Unico") {
+        $sql = $this->conectar()->prepare("select * from $tabla where $campo = :id");
+        $sql->bindParam(":id", $id, PDO::PARAM_INT);
+      } elseif ($tipo == "Normal") {
+        $sql = $this->conectar()->prepare("select $campo from $tabla");
+      }
 
-        try {
-            $sql = $this->conectar()->prepare($query);
-
-            foreach ($datos as $clave) {
-                $sql->bindParam($clave["campo_marcador"], $clave["campo_valor"]);
-            }
-            $sql->bindParam($condicion["condicion_marcador"], $condicion["condicion_valor"]);
-
-            $sql->execute();
-        } catch (Exception) {
-            $sql = null;
-        }
-
-        return $sql;
+      $sql->execute();
+    } catch (Exception) {
+      $sql = null;
     }
 
-    protected function eliminarRegistro($tabla, $campo, $id)
-    {
-        try {
-            $sql = $this->conectar()->prepare("delete from $tabla where $campo= :id");
-            $sql->bindParam(":id", $id);
-            $sql->execute();
-        } catch (Exception) {
-            $sql = null;
-        }
+    return $sql;
+  }
 
-        return $sql;
+  protected function actualizarDatos($tabla, $datos, $condicion)
+  {
+    $query = "update $tabla set ";
+
+    $contador = 0;
+    foreach ($datos as $clave) {
+      if ($contador >= 1) {
+        $query .= ",";
+      }
+      $query .= $clave["campo_nombre"] . "=" . $clave["campo_marcador"];
+      $contador++;
     }
 
-    protected function paginadorTablas($pagina, $numeroPaginas, $url, $botones)
-    {
-        $tabla = '<nav class="pagination is-centered is-rounded" role="navigation" aria-label="pagination">';
+    $query .= " where " . $condicion["condicion_campo"] . "=" . $condicion["condicion_marcador"];
 
-        if ($pagina <= 1) {
-            $tabla .= '
+    try {
+      $sql = $this->conectar()->prepare($query);
+
+      foreach ($datos as $clave) {
+        $sql->bindParam($clave["campo_marcador"], $clave["campo_valor"]);
+      }
+      $sql->bindParam($condicion["condicion_marcador"], $condicion["condicion_valor"]);
+
+      $sql->execute();
+    } catch (Exception) {
+      $sql = null;
+    }
+
+    return $sql;
+  }
+
+  protected function eliminarRegistro($tabla, $campo, $id)
+  {
+    try {
+      $sql = $this->conectar()->prepare("delete from $tabla where $campo= :id");
+      $sql->bindParam(":id", $id);
+      $sql->execute();
+    } catch (Exception) {
+      $sql = null;
+    }
+
+    return $sql;
+  }
+
+  protected function empaquetarDatos($camposTabla, $datos)
+  {
+    if (sizeof($camposTabla) != sizeof($datos)) return null;
+
+    $datosEmpaquetados = array();
+
+    $contador = 0;
+    foreach ($camposTabla as $campoTabla) {
+      array_push(
+        $datosEmpaquetados,
+        [
+          "campo_nombre" => $campoTabla,
+          "campo_marcador" => ":" . $campoTabla,
+          "campo_valor" => $datos[$contador]
+        ]
+      );
+
+      $contador++;
+    }
+
+    return $datosEmpaquetados;
+  }
+
+  protected function paginadorTablas($pagina, $numeroPaginas, $url, $botones)
+  {
+    $tabla = '<nav class="pagination is-centered is-rounded" role="navigation" aria-label="pagination">';
+
+    if ($pagina <= 1) {
+      $tabla .= '
             <a class="pagination-previous is-disabled" disabled >Anterior</a>
             <ul class="pagination-list">
             ';
-        } else {
-            $tabla .= '
+    } else {
+      $tabla .= '
             <a class="pagination-previous" href="' . $url . ($pagina - 1) . '/">Anterior</a>
             <ul class="pagination-list">
                 <li><a class="pagination-link" href="' . $url . '1/">1</a></li>
                 <li><span class="pagination-ellipsis">&hellip;</span></li>
             ';
-        }
+    }
 
 
-        $ci = 0;
-        for ($i = $pagina; $i <= $numeroPaginas; $i++) {
+    $ci = 0;
+    for ($i = $pagina; $i <= $numeroPaginas; $i++) {
 
-            if ($ci >= $botones) {
-                break;
-            }
+      if ($ci >= $botones) {
+        break;
+      }
 
-            if ($pagina == $i) {
-                $tabla .= '<li><a class="pagination-link is-current" href="' . $url . $i . '/">' . $i . '</a></li>';
-            } else {
-                $tabla .= '<li><a class="pagination-link" href="' . $url . $i . '/">' . $i . '</a></li>';
-            }
+      if ($pagina == $i) {
+        $tabla .= '<li><a class="pagination-link is-current" href="' . $url . $i . '/">' . $i . '</a></li>';
+      } else {
+        $tabla .= '<li><a class="pagination-link" href="' . $url . $i . '/">' . $i . '</a></li>';
+      }
 
-            $ci++;
-        }
+      $ci++;
+    }
 
 
-        if ($pagina == $numeroPaginas) {
-            $tabla .= '
+    if ($pagina == $numeroPaginas) {
+      $tabla .= '
             </ul>
             <a class="pagination-next is-disabled" disabled >Siguiente</a>
             ';
-        } else {
-            $tabla .= '
+    } else {
+      $tabla .= '
                 <li><span class="pagination-ellipsis">&hellip;</span></li>
                 <li><a class="pagination-link" href="' . $url . $numeroPaginas . '/">' . $numeroPaginas . '</a></li>
             </ul>
             <a class="pagination-next" href="' . $url . ($pagina + 1) . '/">Siguiente</a>
             ';
-        }
-
-        $tabla .= '</nav>';
-        return $tabla;
     }
 
-    protected function crearAlertaError($mensajeAlerta)
-    {
-        $alerta = [
-            "tipo" => "simple",
-            "titulo" => "Ocurrió un error inesperado",
-            "texto" => $mensajeAlerta,
-            "icono" => "error"
-        ];
+    $tabla .= '</nav>';
+    return $tabla;
+  }
 
-        return $alerta;
-    }
+  protected function crearAlertaError($mensajeAlerta)
+  {
+    $alerta = [
+      "tipo" => "simple",
+      "titulo" => "Ocurrió un error inesperado",
+      "texto" => $mensajeAlerta,
+      "icono" => "error"
+    ];
 
-    protected function crearAlertaLimpiarSuccess($titulo, $mensajeAlerta)
-    {
-        $alerta = [
-            "tipo" => "limpiar",
-            "titulo" => $titulo,
-            "texto" => $mensajeAlerta,
-            "icono" => "success"
-        ];
+    return $alerta;
+  }
 
-        return $alerta;
-    }
+  protected function crearAlertaLimpiarSuccess($titulo, $mensajeAlerta)
+  {
+    $alerta = [
+      "tipo" => "limpiar",
+      "titulo" => $titulo,
+      "texto" => $mensajeAlerta,
+      "icono" => "success"
+    ];
 
-    protected function mostrarError($mensajeAlerta)
-    {
-        return "
+    return $alerta;
+  }
+
+  protected function mostrarError($mensajeAlerta)
+  {
+    return "
             <script>
                 Swal.fire({
                     icon: 'error',
@@ -287,17 +310,17 @@ class mainModel
                 });
             </script>
         ";
-    }
+  }
 
-    protected function crearAlertaRecargar($titulo, $mensajeAlerta)
-    {
-        $alerta = [
-            "tipo" => "recargar",
-            "titulo" => $titulo,
-            "texto" => $mensajeAlerta,
-            "icono" => "success"
-        ];
+  protected function crearAlertaRecargar($titulo, $mensajeAlerta)
+  {
+    $alerta = [
+      "tipo" => "recargar",
+      "titulo" => $titulo,
+      "texto" => $mensajeAlerta,
+      "icono" => "success"
+    ];
 
-        return $alerta;
-    }
+    return $alerta;
+  }
 }
