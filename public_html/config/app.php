ea8b5aa75ff5d1a95d86c25dac9230743ccc1225
Xanderitsme <xanderitsme@gmail.com>
2023-12-14 14:30:44 -0500

    Actualiza la ruta absoluta de la aplicación

--- public_html/config/app.php
+++ public_html/config/app.php
@@ -1,5 +1,5 @@
 <?php
-const APP_URL = "http://localhost/deco_hogar/public_html/";
+const APP_URL = "https://deco-hogar-app.000webhostapp.com/";
 const APP_NAME = "Deco Hogar";
 const APP_SESION_NAME = "APP";
 
