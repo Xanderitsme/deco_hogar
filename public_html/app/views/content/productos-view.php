51e77ff7c9189eba92809eef8485a1fc6ce3aa1b
Xanderitsme <xanderitsme@gmail.com>
2023-12-15 15:14:03 -0500

    Modifica nombre del controlador producto

--- public_html/app/views/content/productos-view.php
+++ public_html/app/views/content/productos-view.php
@@ -1,8 +1,8 @@
 <?php
 
-use app\controllers\usuarioController;
+use app\controllers\productController;
 
-$insUsuario = new usuarioController();
+$insProducto = new productController();
 ?>
 <style>
 	body,
@@ -13,18 +13,18 @@ $insUsuario = new usuarioController();
 <div class="seccion-productos">
 	<div class="panel-izquierdo">
 		<h2 class="subtitle">Productos disponibles</h2>
-		<form action="<?php echo APP_URL; ?>app/ajax/usuarioAjax.php" method="post">
+		<form action="<?php echo APP_URL; ?>app/ajax/productoAjax.php" method="post">
 			<input class="borde sombra buscador" type="text" placeholder="Buscar productos...">
 			<input type="hidden" name="buscador_productos" value="buscar_producto">
 		</form>
 		<?php
-		echo $insUsuario->listarProductosVentaControlador("");
+		echo $insProducto->listarProductosVentaControlador("");
 		?>
 	</div>
 	<div class="panel-derecho">
 		<h2 class="subtitle">Productos a vender</h2>
 		<?php
-		echo $insUsuario->listarProductosProformaControlador(1);
+		echo $insProducto->listarProductosProformaControlador(1);
 		?>
 	</div>
 </div>
