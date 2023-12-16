e7b1046c89a49ead7b72064a3e627e5c4b652b29
Xanderitsme <xanderitsme@gmail.com>
2023-12-16 10:48:03 -0500

    Optimiza el código para registrar y eliminar

--- public_html/app/controllers/trabajadorController.php
+++ public_html/app/controllers/trabajadorController.php
@@ -3,9 +3,9 @@
 namespace app\controllers;
 
 use app\models\mainModel;
+use app\models\trabajadorModel;
 use DateTime;
 use PDO;
-
 
 class trabajadorController extends mainModel
 {
@@ -14,15 +14,14 @@ class trabajadorController extends mainModel
     $nombres = $this->limpiarCadena($_POST["trabajador_nombres"]);
     $apellidos = $this->limpiarCadena($_POST["trabajador_apellidos"]);
     $dni = $this->limpiarCadena($_POST["trabajador_dni"]);
-    $email = $this->limpiarCadena($_POST["trabajador_email"]);
     $telefono = $this->limpiarCadena($_POST["trabajador_telefono"]);
+    $email = $this->limpiarCadena($_POST["trabajador_email"]);
     $fechaTemp = $this->limpiarCadena($_POST["trabajador_fecha_contratacion"]);
     $fechaContratacion = DateTime::createFromFormat('Y-m-d', $fechaTemp)->format('Y-m-d');
     $sueldo = $this->limpiarCadena($_POST["trabajador_sueldo"]);
-    $cargo = $this->limpiarCadena($_POST["trabajador_cargo"]);
-    $cargoId = "";
+    $cargoId = $this->limpiarCadena($_POST["trabajador_cargo"]);
 
-    if (empty($nombres) || empty($apellidos) || empty($dni) || empty($fechaContratacion) || empty($sueldo) || empty($cargo)) {
+    if (empty($nombres) || empty($apellidos) || empty($dni) || empty($fechaContratacion) || empty($sueldo) || empty($cargoId)) {
       $alerta = $this->crearAlertaError("No has llenado todos los campos que son obligatorios");
       return json_encode($alerta);
       exit();
@@ -58,128 +57,74 @@ class trabajadorController extends mainModel
       exit();
     }
 
-    if ($this->verificarDatos("[a-zA-ZáéíóúÁÉÍÓÚñÑ ]{3,40}", $cargo)) {
+    if ($this->verificarDatos("[1-9]{1,3}", $cargoId)) {
       $alerta = $this->crearAlertaError("El cargo no coincide con el formato solicitado");
       return json_encode($alerta);
       exit();
-    } else {
-      $check_cargo = $this->ejecutarConsulta("select ID from cargos where nombre_cargo = '$cargo'");
-
-      if (!is_null($check_cargo)) {
-        if ($check_cargo->rowCount() > 0) {
-          $resultado = $check_cargo->fetch(PDO::FETCH_ASSOC);
-          $cargoId = $resultado["ID"];
-        } else {
-          $alerta = $this->crearAlertaError("El cargo ingresado no existe");
-          return json_encode($alerta);
-          exit();
-        }
-      } else {
-        return $this->errorRegistroTrabajador();
-        exit();
-      }
     }
 
-    if (!empty($email)) {
-      if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
-        $check_email = $this->ejecutarConsulta("select email from trabajadores where email = '$email'");
+    $check_cargo = $this->ejecutarConsulta("select ID from cargos where ID = '$cargoId'");
 
-        if (!is_null($check_email)) {
-          if ($check_email->rowCount() > 0) {
-            $alerta = $this->crearAlertaError("El email que acaba de ingresar ya se encuentra registrado");
-            return json_encode($alerta);
-            exit();
-          }
-        } else {
-          return $this->errorRegistroTrabajador();
-          exit();
-        }
-      } else {
+    if (is_null($check_cargo)) {
+      return $this->errorRegistroTrabajador();
+      exit();
+    }
+
+    if ($check_cargo->rowCount() == 0) {
+      $alerta = $this->crearAlertaError("El cargo ingresado no existe");
+      return json_encode($alerta);
+      exit();
+    }
+
+    if (empty($email)) {
+      $email = null;
+    } else {
+      if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
         $alerta = $this->crearAlertaError("Ha ingresado un email no válido");
         return json_encode($alerta);
         exit();
       }
-    } else {
-      $email = null;
+
+      $check_email = $this->ejecutarConsulta("select email from trabajadores where email = '$email'");
+
+      if (is_null($check_email)) {
+        return $this->errorRegistroTrabajador();
+        exit();
+      }
+
+      if ($check_email->rowCount() > 0) {
+        $alerta = $this->crearAlertaError("El email que acaba de ingresar ya se encuentra registrado");
+        return json_encode($alerta);
+        exit();
+      }
     }
 
-    if (!empty($telefono)) {
+    if (empty($telefono)) {
+      $telefono = null;
+    } else {
       if ($this->verificarDatos("[0-9]{9}", $telefono)) {
         $alerta = $this->crearAlertaError("Ha ingresado un numero de celular no no válido");
         return json_encode($alerta);
         exit();
-      } else {
-        $check_telefono = $this->ejecutarConsulta("select telefono from trabajadores where telefono = '$telefono'");
-
-        if (!is_null($check_telefono)) {
-          if ($check_telefono->rowCount() > 0) {
-            $alerta = $this->crearAlertaError("El numero de celular que acaba de ingresar ya se encuentra registrado");
-            return json_encode($alerta);
-            exit();
-          }
-        } else {
-          return $this->errorRegistroTrabajador();
-          exit();
-        }
       }
-    } else {
-      $telefono = null;
+
+      $check_telefono = $this->ejecutarConsulta("select telefono from trabajadores where telefono = '$telefono'");
+
+      if (is_null($check_telefono)) {
+        return $this->errorRegistroTrabajador();
+        exit();
+      }
+
+      if ($check_telefono->rowCount() > 0) {
+        $alerta = $this->crearAlertaError("El numero de celular que acaba de ingresar ya se encuentra registrado");
+        return json_encode($alerta);
+        exit();
+      }
     }
 
-    $trabajador_datos_reg = [
-      [
-        "campo_nombre" => "nombres",
-        "campo_marcador" => ":nombres",
-        "campo_valor" => $nombres
-      ],
-      [
-        "campo_nombre" => "apellidos",
-        "campo_marcador" => ":apellidos",
-        "campo_valor" => $apellidos
-      ],
-      [
-        "campo_nombre" => "dni",
-        "campo_marcador" => ":dni",
-        "campo_valor" => $dni
-      ],
-      [
-        "campo_nombre" => "telefono",
-        "campo_marcador" => ":telefono",
-        "campo_valor" => $telefono
-      ],
-      [
-        "campo_nombre" => "email",
-        "campo_marcador" => ":email",
-        "campo_valor" => $email
-      ],
-      [
-        "campo_nombre" => "fecha_contratacion",
-        "campo_marcador" => ":fechaContratacion",
-        "campo_valor" => $fechaContratacion
-      ],
-      [
-        "campo_nombre" => "sueldo",
-        "campo_marcador" => ":sueldo",
-        "campo_valor" => $sueldo
-      ],
-      [
-        "campo_nombre" => "cargoID",
-        "campo_marcador" => ":cargoId",
-        "campo_valor" => $cargoId
-      ],
-    ];
+    $insTrabajador = new trabajadorModel();
 
-    $registrarTrabajador = $this->guardarDatos("trabajadores", $trabajador_datos_reg);
-
-    if (!is_null($registrarTrabajador)) {
-      if ($registrarTrabajador->rowCount() == 1) {
-        $alerta = $this->crearAlertaLimpiarSuccess("Trabajador registrado", "El trabajador " . $nombres . " " . $apellidos . " ha sido registrado exitosamente");
-      } else {
-        $alerta = $this->crearAlertaError("No se pudo registrar el trabajador, por favor intente nuevamente");
-      }
-    } else {
-      $alerta = $this->crearAlertaError("No se pudo registrar el trabajador, por favor intente más tarde");
-    }
+    $alerta = $insTrabajador->registrar([$nombres, $apellidos, $dni, $telefono, $email, $fechaContratacion, $sueldo, $cargoId]);
 
     return json_encode($alerta);
   }
@@ -285,8 +230,8 @@ class trabajadorController extends mainModel
           <thead>
             <tr>
               <th class="has-text-centered">#</th>
-              <th class="has-text-centered">Nombres</th>
-              <th class="has-text-centered">Apellidos</th>
+              <th class="has-text-centered">Nombres y apellidos</th>
+              
               <th class="has-text-centered">DNI</th>
               <th class="has-text-centered">Teléfono</th>
               <th class="has-text-centered">Correo electrónico</th>
@@ -307,8 +252,7 @@ class trabajadorController extends mainModel
         $tabla .= '
                 <tr class="has-text-centered" >
                     <td>' . $contador . '</td>
-                    <td>' . $rows['nombres'] . '</td>
-                    <td>' . $rows['apellidos'] . '</td>
+                    <td>' . $rows['nombres'] . ' ' . $rows['apellidos'] . '</td>
                     <td>' . $rows['dni'] . '</td>
                     <td>' . $rows['telefono'] . '</td>
                     <td>' . $rows['email'] . '</td>
@@ -319,7 +263,7 @@ class trabajadorController extends mainModel
                         <a href="' . APP_URL . 'actualizarTrabajador/' . $rows['ID'] . '/" class="button is-success is-rounded is-small">Actualizar</a>
                     </td>
                     <td>
-                        <form class="FormularioAjax" action="' . APP_URL . 'app/ajax/usuarioAjax.php" method="POST" autocomplete="off" >
+                        <form class="FormularioAjax" action="' . APP_URL . 'app/ajax/trabajadorAjax.php" method="POST" autocomplete="off" >
 
                             <input type="hidden" name="modulo_trabajador" value="eliminar">
                             <input type="hidden" name="trabajador_id" value="' . $rows['ID'] . '">
@@ -337,7 +281,7 @@ class trabajadorController extends mainModel
       if ($total >= 1) {
         $tabla .= '
                 <tr class="has-text-centered" >
-                    <td colspan="11">
+                    <td colspan="10">
                         <a href="' . $url . '1/" class="button is-link is-rounded is-small mt-4 mb-4">
                             Haga clic acá para recargar el listado
                         </a>
@@ -347,7 +291,7 @@ class trabajadorController extends mainModel
       } else {
         $tabla .= '
                 <tr class="has-text-centered" >
-                    <td colspan="11">
+                    <td colspan="10">
                         No hay registros en el sistema
                     </td>
                 </tr>
@@ -376,65 +320,37 @@ class trabajadorController extends mainModel
     $id = $this->limpiarCadena($_POST['trabajador_id']);
 
     if ($id == 1) {
-      $alerta = $this->crearAlertaError('No podemos eliminar la cuenta del usuario principal');
+      $alerta = $this->crearAlertaError("No podemos eliminar la cuenta del usuario principal");
       return json_encode($alerta);
       exit();
     }
 
-    $datosTrabajador = $this->ejecutarConsulta("
-      select 
-        trabajadores.nombres,
-        trabajadores.apellidos 
-      from trabajadores 
-      where trabajadores.ID <> 1 
-      and trabajadores.ID = $id
-    ");
+    $insTrabajador = new trabajadorModel();
 
-    $datosCuenta = $this->ejecutarConsulta("
-      select 
-        cuentas.foto
-      from cuentas 
-      where cuentas.ID <> 1 
-      and cuentas.trabajadorID = $id
-    ");
-
-    if (is_null($datosTrabajador) || is_null($datosCuenta)) {
-      $alerta = $this->crearAlertaError('Ha ocurrido un error al intentar cargar los datos del trabajador');
-      return json_encode($alerta);
-      exit();
-    }
-
-    if ($datosTrabajador->rowCount() == 0) {
-      $alerta = $this->crearAlertaError('No pudimos encontrar ese trabajador en el sistema');
-      return json_encode($alerta);
-      exit();
-    }
-
-    $datosTrabajador = $datosTrabajador->fetch(PDO::FETCH_ASSOC);
-
-    $eliminarTrabajador = $this->eliminarRegistro("trabajadores", "ID", $id);
-
-    if (is_null($eliminarTrabajador)) {
-      $alerta = $this->crearAlertaError('Ha ocurrido un error al intentar eliminar el usuario');
-      return json_encode($alerta);
-      exit();
-    }
-
-    if ($eliminarTrabajador->rowCount() == 1) {
-      if ($datosCuenta->rowCount() > 0) {
-        $datosCuenta = $datosCuenta->fetch(PDO::FETCH_ASSOC);
-        if (is_file("../views/fotos/" . $datosCuenta['foto'])) {
-          chmod("../views/fotos/" . $datosCuenta['foto'], 0777);
-          unlink("../views/fotos/" . $datosCuenta['foto']);
-        }
-      }
-
-      $alerta = $this->crearAlertaRecargar("Trabajador eliminado", "El trabajador " . $datosTrabajador['nombres'] . " " . $datosTrabajador['apellidos'] . " se eliminó con éxito");
-    } else {
-      $alerta = $this->crearAlertaError("No se pudo eliminar el usuario "  . $datosTrabajador['nombres'] . " " . $datosTrabajador['apellidos'] . ", por favor intente nuevamente");
-    }
+    $alerta = $insTrabajador->eliminar($id);
 
     return json_encode($alerta);
+  }
+
+  public function obtenerCargos()
+  {
+    $listaCargos = '';
+
+    $consulta_cargos = "select ID, nombre_cargo from cargos order by nombre_cargo desc";
+
+    $cargos = $this->ejecutarConsulta($consulta_cargos);
+
+    if (is_null($cargos)) {
+      return $this->mostrarError("Ha ocurrido un error al intentar cargar los cargos");
+    }
+
+    $cargos = $cargos->fetchAll();
+
+    foreach ($cargos as $cargo) {
+      $listaCargos .= '<option value="' . $cargo['ID'] . '">' . $cargo['nombre_cargo'] . '</option>';
+    }
+
+    return $listaCargos;
   }
 
   private function errorRegistroTrabajador()
