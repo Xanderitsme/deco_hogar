133302d1b02005446c2ba69f9132fe1e096facd2
Xanderitsme <xanderitsme@gmail.com>
2023-12-16 16:43:44 -0500

    Agrega vista para registrar una proforma de venta

---
+++ public_html/app/views/content/nuevaProformaVenta-view.php
@@ -0,0 +1,30 @@
+<?php
+
+use app\controllers\productController;
+
+$insProducto = new productController();
+?>
+<style>
+	body,
+	html {
+		overflow: hidden;
+	}
+</style>
+<div class="seccion-productos">
+	<div class="panel-izquierdo">
+		<h2 class="subtitle">Productos disponibles</h2>
+		<form action="<?php echo APP_URL; ?>app/ajax/productoAjax.php" method="post">
+			<input class="borde sombra buscador" type="text" placeholder="Buscar productos...">
+			<input type="hidden" name="buscador_productos" value="buscar_producto">
+		</form>
+		<?php
+		echo $insProducto->listarProductosVentaControlador("");
+		?>
+	</div>
+	<div class="panel-derecho">
+		<h2 class="subtitle">Proforma de venta</h2>
+		<?php
+		echo $insProducto->listarProductosProformaControlador(1);
+		?>
+	</div>
+</div>
