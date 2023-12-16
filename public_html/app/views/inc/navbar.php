0c467954ee43d70212910d7090aa7f02913b0830
Xanderitsme <xanderitsme@gmail.com>
2023-12-16 16:49:10 -0500

    Modifica la estructura de la navbar
    Se modificó la navbar para poder colocar los enlaces de todas las secciones de la aplicación

--- public_html/app/views/inc/navbar.php
+++ public_html/app/views/inc/navbar.php
@@ -3,74 +3,103 @@
     <a class="navbar-item" href="<?php echo APP_URL; ?>dashboard/">
       <h1 class="title" style="font-weight: 700;">Deco Hogar</h1>
     </a>
-    <div class="navbar-burger" data-target="navbarExampleTransparentExample">
+    <div class="navbar-burger" data-target="navbar-links">
       <span></span>
       <span></span>
       <span></span>
     </div>
   </div>
 
-  <div id="navbarExampleTransparentExample" class="navbar-menu">
+  <div id="navbar-links" class="navbar-menu">
     <div class="navbar-start">
-      <a class="navbar-item" href="<?php echo APP_URL; ?>dashboard/">
-        Dashboard
-      </a>
+
       <div class="navbar-item has-dropdown is-hoverable">
-        <a class="navbar-link" href="<?php echo APP_URL; ?>productos/">
-          Productos
-        </a>
-        <div class="navbar-dropdown is-boxed">
-          <a class="navbar-item" href="<?php echo APP_URL; ?>nuevoProducto/">
-            Registrar nuevo producto
-          </a>
-        </div>
-      </div>
-      <div class="navbar-item has-dropdown is-hoverable">
-        <a class="navbar-link" href="<?php echo APP_URL; ?>clientes/">
-          Clientes
-        </a>
-        <div class="navbar-dropdown is-boxed">
-          <a class="navbar-item" href="#">
-            Nuevo cliente
-          </a>
-        </div>
-      </div>
-      <div class="navbar-item has-dropdown is-hoverable">
-        <a class="navbar-link" href="#">
+        <a class="navbar-link" href="<?php echo APP_URL; ?>listaProformasVenta/">
           Proformas de venta
         </a>
         <div class="navbar-dropdown is-boxed">
-          <a class="navbar-item" href="#">
+          <a class="navbar-item" href="<?php echo APP_URL; ?>nuevaProformaVenta/">
             Nueva proforma de venta
+          </a>
+          <a class="navbar-item" href="<?php echo APP_URL; ?>listaProformasVenta/">
+            Lista de proformas de venta
           </a>
         </div>
       </div>
+
       <div class="navbar-item has-dropdown is-hoverable">
-        <a class="navbar-link" href="#">
+        <a class="navbar-link" href="<?php echo APP_URL; ?>listaClientes/">
+          Clientes
+        </a>
+        <div class="navbar-dropdown is-boxed">
+          <a class="navbar-item" href="<?php echo APP_URL; ?>nuevoCliente/">
+            Nuevo cliente
+          </a>
+          <a class="navbar-item" href="<?php echo APP_URL; ?>listaClientes/">
+            Lista de clientes
+          </a>
+        </div>
+      </div>
+
+      <div class="navbar-item has-dropdown is-hoverable">
+        <a class="navbar-link" href="<?php echo APP_URL; ?>listaMovimientosInventario/">
           Inventario
         </a>
         <div class="navbar-dropdown is-boxed">
-          <a class="navbar-item" href="#">
-            Historial de movimientos de inventario
+          <a class="navbar-item" href="<?php echo APP_URL; ?>listaMovimientosInventario/">
+            Lista de movimientos de inventario
+          </a>
+          <a class="navbar-item" href="<?php echo APP_URL; ?>nuevoProducto/">
+            Nuevo producto
+          </a>
+          <a class="navbar-item" href="<?php echo APP_URL; ?>listaProductos/">
+            Lista de productos
           </a>
         </div>
       </div>
+
+      <div class="navbar-item has-dropdown is-hoverable">
+        <a class="navbar-link" href="<?php echo APP_URL; ?>listaOrdenesCompra/">
+          Ordenes de compra
+        </a>
+        <div class="navbar-dropdown is-boxed">
+          <a class="navbar-item" href="<?php echo APP_URL; ?>nuevaOrdenCompra/">
+            Nueva orden de compra
+          </a>
+          <a class="navbar-item" href="<?php echo APP_URL; ?>listaOrdenesCompra/">
+            Lista de ordenes de compra
+          </a>
+        </div>
+      </div>
+
       <div class="navbar-item has-dropdown is-hoverable">
         <a class="navbar-link" href="<?php echo APP_URL; ?>listaTrabajadores/">
           Trabajadores
         </a>
         <div class="navbar-dropdown is-boxed">
           <a class="navbar-item" href="<?php echo APP_URL; ?>nuevoTrabajador/">
-            Registrar trabajador
+            Nuevo trabajador
           </a>
-          <a class="navbar-item" href="<?php echo APP_URL; ?>listaUsuarios/">
-            Usuarios
-          </a>
-          <a class="navbar-item" href="<?php echo APP_URL; ?>nuevoUsuario/">
-            Registrar usuario
+          <a class="navbar-item" href="<?php echo APP_URL; ?>listaTrabajadores/">
+            Lista de trabajadores
           </a>
         </div>
       </div>
+
+      <div class="navbar-item has-dropdown is-hoverable">
+        <a class="navbar-link" href="<?php echo APP_URL; ?>listaUsuarios/">
+          Usuarios
+        </a>
+        <div class="navbar-dropdown is-boxed">
+          <a class="navbar-item" href="<?php echo APP_URL; ?>nuevoUsuario/">
+            Nuevo usuario
+          </a>
+          <a class="navbar-item" href="<?php echo APP_URL; ?>listaUsuarios/">
+            Lista de usuarios
+          </a>
+        </div>
+      </div>
+
     </div>
 
     <div class="navbar-end">
