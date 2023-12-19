e1b16360ce9de4e872558783fe64061550d68eea
Xanderitsme <xanderitsme@gmail.com>
2023-12-19 13:32:33 -0500

    Implementa define automático de datos BD conexión

--- public_html/config/server.php
+++ public_html/config/server.php
@@ -1,9 +1,15 @@
 <?php
-const DB_SERVER = "localhost";
-const DB_NAME = "id21665487_deco_hogar_db";
-const DB_USER = "id21665487_administrator";
-const DB_PASS = "w@GrHJK!!79w";
 
-// const DB_NAME = "deco_hogar";
-// const DB_USER = "root";
-// const DB_PASS = "root";
+if ($_SERVER['SERVER_ADDR'] === '127.0.0.1' || $_SERVER['SERVER_ADDR'] === '::1') {
+  // Entorno local
+  define('DB_SERVER', 'localhost');
+  define('DB_NAME', 'deco_hogar');
+  define('DB_USER', 'root');
+  define('DB_PASS', 'root');
+} else {
+  // Entorno remoto
+  define('DB_SERVER', 'localhost');
+  define('DB_NAME', 'id21665487_deco_hogar_db');
+  define('DB_USER', 'id21665487_administrator');
+  define('DB_PASS', 'w@GrHJK!!79w');
+}
