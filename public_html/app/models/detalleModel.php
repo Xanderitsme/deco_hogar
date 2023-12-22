e60655371bcfc6da73fcfb16f5bdd1afd4c1e01f
Xanderitsme <xanderitsme@gmail.com>
2023-12-21 23:11:10 -0500

    Implementa métodos actualizar y eliminar

--- public_html/app/models/detalleModel.php
+++ public_html/app/models/detalleModel.php
@@ -11,7 +11,6 @@ interface Detalle extends Objeto
   const CAMPOS_TABLA = [
     "cantidad",
     "subtotal",
-    "unidad",
     "productoID",
     "proforma_ventaID"
   ];
@@ -43,9 +42,10 @@ class detalleModel extends mainModel implements Detalle
       select
         detalles.cantidad,
         detalles.subtotal,
-        detalles.unidad,
+        detalles.proforma_ventaID,
         productos.nombre,
-        detalles.proforma_ventaID
+        productos.precio_venta,
+        productos.stock
       from detalles
       inner join productos on detalles.productoID = productos.ID
       where detalles.ID = $id
@@ -60,10 +60,30 @@ class detalleModel extends mainModel implements Detalle
 
   public function actualizar($id, $datos)
   {
-    
+    $datosDetalle = $this->empaquetarDatos(self::CAMPOS_TABLA, $datos);
+
+    if (is_null($datosDetalle))
+      return false;
+
+    $condicion = $this->empaquetarCondicion("ID", $id);
+
+    $actualizar = $this->actualizarDatos(self::NOMBRE_TABLA, $datosDetalle, $condicion);
+
+    if (is_null($actualizar)) {
+      return false;
+    }
+
+    if ($actualizar->rowCount() == 0) {
+      return false;
+    }
+
+    return true;
   }
 
   public function eliminar($id)
   {
+    $eliminarDetalle = $this->eliminarRegistro(self::NOMBRE_TABLA, 'ID', $id);
+
+    return ($eliminarDetalle->rowCount() != 0);
   }
 }
